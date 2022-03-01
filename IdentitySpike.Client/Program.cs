9b41905c35554a0a7916ba299126ce9021f2519a
bob h <mail@bob.codes>
2022-03-01 15:24:29 +0000

    creating the client
    https://docs.identityserver.io/en/latest/quickstarts/1_client_credentials.html#creating-the-client

---
+++ IdentitySpike.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using IdentityModel.Client;
+
+using var client = new HttpClient();
+
+using var cts = new CancellationTokenSource(millisecondsDelay: 30_000);
+
+while (!cts.IsCancellationRequested)
+{
+	var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+	Console.WriteLine("{0} {1}", DateTime.UtcNow, disco.Error);
+	Console.WriteLine("{0} {1}", DateTime.UtcNow, disco.ClaimsSupported);
+
+	await Task.Delay(millisecondsDelay: 1_000);
+}
+
+Console.ReadKey();
